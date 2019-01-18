b482ccd040b3d9427b5de24d2ee5cfa044362331
jvmalone <jvmalone@mac.com>
2019-01-18 15:44:42 -0500

    Initial Commit

---
+++ testApplication/testApplication.h
@@ -0,0 +1,19 @@
+//
+//  testApplication.h
+//  testApplication
+//
+//  Created by Joe Malone on 1/18/19.
+//  Copyright © 2019 Joe Malone. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for testApplication.
+FOUNDATION_EXPORT double testApplicationVersionNumber;
+
+//! Project version string for testApplication.
+FOUNDATION_EXPORT const unsigned char testApplicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testApplication/PublicHeader.h>
+
+
